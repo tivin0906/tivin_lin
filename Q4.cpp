1f51fce0f6e66af48b79631e1cf1acb41fa86494
Tivin_Lin <tivin_lin@htc.com>
2016-05-26 17:35:04 +0800

    [HW#4][Modify] 1. Generate random number list print function 2. Generate a function that can change specific place value in random number list

--- Q4.cpp
+++ Q4.cpp
@@ -6,22 +6,28 @@
 #define MAX 412
 #define SIZE MAX-MIN+1
 #define DRAW 18
+#define POSITION 3
+#define ReplaceNum 500
 
 int *generate_random_list();
 int choose(int *, int);
+void show(int *, int);
+int replace(int *, int, int);
 
 int main (void)
 {  
 	int *random_array;
-	int i=0, position=3;
+	int i=0;
 	
 	random_array = generate_random_list();
 	
 	printf("Draw %d Random number from %d to %d  :\n", DRAW, MIN, MAX);
-	for(i=0; i<DRAW; i++)
-		printf("%03d ", *(random_array+i));
+	show(random_array, DRAW);
 	
-	printf("\n\n%d-th Number is %d", position, choose(random_array, position));
+	printf("\n\n%d-th number %d is replaced by %d.\n\n", POSITION, choose(random_array, POSITION), ReplaceNum);
+	printf("Replaced array : \n");
+	replace(random_array, POSITION, ReplaceNum);
+	show(random_array, DRAW);
 	
     return 0; 
 }
@@ -43,7 +49,7 @@ int *generate_random_list()
 		*(arr+rand_num) = *(arr+(array_size-1));	// rand_num-th number is replaced by the last number
 		array_size--;								// Reduce the range of random number
 	}
-	delete [] arr;
+	delete [] arr;									// delete *arr
 	return rand_arr;
 }
 
@@ -52,3 +58,15 @@ int choose(int *array, int index)
 	return *(array+(index-1));
 }
 
+void show(int *array, int draw)
+{
+	int i=0;
+	
+	for(i=0; i<draw; i++)
+		printf("%03d ", *(array+i));
+}
+
+int replace(int *array, int index, int value)
+{
+	return *(array+(index-1)) = value;
+}
